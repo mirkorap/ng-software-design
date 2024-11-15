e859f2152e914c04e718f722122c67d0e3f57572
mirkorap16 <m.rapisarda@acadevmy.it>
2024-11-15 22:23:17 +0100

    feat: register i18nService for ui-kit

--- ng-software-design/src/app/app.component.ts
+++ ng-software-design/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { I18nService } from 'core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   imports: [RouterOutlet],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(i18nService: I18nService) {
+    i18nService.setDefaultLang('en');
+    i18nService.setActiveLang('en');
+  }
+}
--- ng-software-design/src/main.ts
+++ ng-software-design/src/main.ts
@@ -2,9 +2,18 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import routes from '@app/app.routes';
 import { AppComponent } from '@app/app.component';
 import { provideRouter } from '@angular/router';
-import { provideCore } from 'core';
-import { provideUi } from 'ui-kit';
+import { NgxI18nService, provideCore } from 'core';
+import { I18nService, provideUi } from 'ui-kit';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes), provideCore(), provideUi()],
+  providers: [
+    provideRouter(routes),
+    provideCore(),
+    provideUi({
+      translator: {
+        provide: I18nService,
+        useClass: NgxI18nService,
+      },
+    }),
+  ],
 }).catch((err) => console.error(err));
